b741792d5f3fa163d7a1b9f0f02cc1a9ccde6ef2
Ryan Morales <ryan.morales@west-mec.org>
2024-10-21 11:53:00 -0700

    added logs for useEffect and useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import UseEffectRenderer from "./components/UseEffectRenderer";
+import UseRefRenderer from "./components/UseRefRenderer.jsx";
 import "./App.css";
 
 function App() {
-  return <UseEffectRenderer />;
+  return (
+    <>
+      <UseEffectRenderer />
+      <UseRefRenderer />
+    </>
+  );
 }
 
 export default App;
