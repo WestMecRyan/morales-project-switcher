dfbd0e6f1d791ec07a1d8c80987d0eda30665834
Ryan Morales <ryan.morales@west-mec.org>
2024-10-23 10:34:35 -0700

    removed pokemon files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import UseEffectRenderer from "./components/UseEffectRenderer";
 import UseRefRenderer from "./components/UseRefRenderer.jsx";
-import MyButton from "./components/MyButton";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <MyButton title="Click Me"></MyButton>
       <UseEffectRenderer />
       <UseRefRenderer />
     </>
